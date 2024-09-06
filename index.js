8c4f966acc816326185b4e4962594d5af17b6274
Joseph Crump <josephcrump79@yahoo.com>
2024-09-05 20:06:33 -0500

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driversNames, name){
+    name = name.toLowerCase () 
+return driversNames.filter(dName => dName.toLowerCase() === name);
+}
+
+function fuzzyMatch(driversNames, name){
+return driversNames.filter(dName => dName.startsWith(name))
+}
+
+function matchName(driversNames, name){
+return driversNames.filter(driver => driver.name === name)   
+}
